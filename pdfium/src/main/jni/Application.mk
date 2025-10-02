a6ecf0f4a95d54f38a678c6294e49bb4192270fd
APP SCSOFT <apps.scsoft@gmail.com>
2025-10-02 15:10:08 +0700

    Added 16KB page size support

--- pdfium/src/main/jni/Application.mk
+++ pdfium/src/main/jni/Application.mk
@@ -1,8 +1,9 @@
 APP_STL := c++_shared
 APP_CPPFLAGS += -fexceptions
+APP_SUPPORT_FLEXIBLE_PAGE_SIZES := true
 
 #For ANativeWindow support
-APP_PLATFORM = android-21
+APP_PLATFORM = android-14
 
 APP_ABI :=  armeabi-v7a \
             arm64-v8a \
